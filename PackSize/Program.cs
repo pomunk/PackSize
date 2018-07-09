ef98dafae5bace34203bba4e233c50dcfb43b6a0
Josh <pomunk@yahoo.com>
2018-07-09 11:57:27 -0600

    First commit

---
+++ PackSize/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PackSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
